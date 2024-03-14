7b58895ecc7c1a14a84f99309c2bff8226096d43
Serhii Filonenko <serhii.filonenko@binary-studio.com>
2024-03-14 11:34:05 +0200

    FE: enable commenting of deactivated items in target script

--- forward_engineering/services/protoScriptGenerationService.js
+++ forward_engineering/services/protoScriptGenerationService.js
@@ -1,7 +1,7 @@
 const { dependencies } = require('../../reverse_engineering/appDependencies');
 const { parseDefinitions, getDefinitionInfo, extractDefinitionsFromProperties } = require('../helpers/DefinitionsHelper');
 const { fixFieldNumbers } = require('../helpers/FieldNumberGenerationHelper');
-const { formatComment } = require('../helpers/utils');
+const { formatComment, wrapInCommentBlock } = require('../helpers/utils');
 
 const PROTO_2_FIELD_RULES = ['required', 'optional', 'repeated'];
 const PROTO_3_FIELD_RULES = ['singular', 'repeated'];
@@ -85,7 +85,7 @@ const getMessageStatement = ({ jsonSchema, spacePrefix = '', protoVersion, inter
         oneOfIndex
     });
 
-    const oneOfStatement = getOneOfStatement(jsonSchema?.oneOf_meta?.name || 'one_of', oneOfFields, spacePrefix + ROW_PREFIX);
+    const oneOfStatement = getOneOfStatement(jsonSchema?.oneOf_meta, oneOfFields, spacePrefix + ROW_PREFIX);
 
     const innerDefinitions = internalDefinitions.filter(def => !def.isTopLevel);
     const messageDefinitions = [...innerDefinitions, ...extractedDefinitions].map(definition => getDefinitionStatements({
@@ -96,7 +96,8 @@ const getMessageStatement = ({ jsonSchema, spacePrefix = '', protoVersion, inter
         modelDefinitions,
         externalDefinitions
     })).join('\n');
-    return [description,
+    const statement = [
+        description,
         `${spacePrefix}message ${jsonSchema.title} {`,
         reservedFieldNumbers,
         reservedFieldNames,
@@ -107,6 +108,8 @@ const getMessageStatement = ({ jsonSchema, spacePrefix = '', protoVersion, inter
     ]
         .filter(row => row !== '')
         .join('\n');
+
+    return jsonSchema.isActivated ? statement : wrapInCommentBlock({ statement, spacePrefix });
 }
 
 const concutFieldsStatements = (fieldsStatements, oneOfStatement, oneOfIndex) => {
@@ -118,15 +121,19 @@ const concutFieldsStatements = (fieldsStatements, oneOfStatement, oneOfIndex) =>
     return statements.join('\n');
 }
 
-const getOneOfStatement = (oneOfName, fields, spacePrefix = '') => {
+const getOneOfStatement = (oneOfMeta, fields, spacePrefix = '') => {
+    const oneOfName = oneOfMeta?.name || 'one_of';
     const _ = dependencies.lodash;
     if (_.isEmpty(fields)) {
         return '';
     }
-    return [`${spacePrefix}oneof ${oneOfName} {`,
+
+    const statement = [`${spacePrefix}oneof ${oneOfName} {`,
     ...fields,
         '}'
     ].join(`\n${spacePrefix}`);
+
+    return oneOfMeta?.isActivated ? statement : wrapInCommentBlock({ statement, spacePrefix });
 }
 
 const getEnumStatement = ({ jsonSchema, spacePrefix = '' }) => {
@@ -178,10 +185,8 @@ const getReservedStatements = (data, spacePrefix) => {
 
 const getFieldsStatement = ({ jsonSchema, spacePrefix, protoVersion, internalDefinitions, modelDefinitions, externalDefinitions, oneOfIndex }) => {
     const _ = dependencies.lodash;
-    const oneOfFields = Object.entries((jsonSchema?.oneOf_meta?.isActivated ? jsonSchema.oneOf : [])
-        .filter(property => property.isActivated)
+    const oneOfFields = Object.entries((jsonSchema.oneOf || [])
         .reduce((properties, property) => ({ ...properties, ...property.properties }), {}))
-        .filter(([key, value]) => value.isActivated)
         .reduce((oneOfProperties, [key, value]) => ({ ...oneOfProperties, [key]: { ...value, parent: 'oneOf' } }), {})
     const {collectionProperties, oneOfProperties} = fixFieldNumbers({fields:{...jsonSchema.properties}, oneOfFields, reservedNumbers:jsonSchema.reservedFieldNumbers, oneOfIndex});
     const messageFieldsStatements = convertFieldsToStatements(
@@ -216,6 +221,9 @@ const convertFieldsToStatements = ({ fields, spacePrefix, protoVersion, internal
         }
         if (hasReferenceError) {
             return `${spacePrefix}/*${getValidatedFieldRule({ fieldRule: field.repetition, protoVersion })}${fieldType} ${fieldName} = ${field.fieldNumber};\tERROR: the field contains an incorrect reference to not existed definition*/`
+        }
+        if (!field.isActivated) {
+            return `${spacePrefix}// ${getValidatedFieldRule({ fieldRule: field.repetition, protoVersion })}${fieldType} ${fieldName} = ${field.fieldNumber}${getFieldOptionsStatement(fieldOptions)}; ${field.description || ''}`
         }
 
         return `${spacePrefix}${getValidatedFieldRule({ fieldRule: field.repetition, protoVersion })}${fieldType} ${fieldName} = ${field.fieldNumber}${getFieldOptionsStatement(fieldOptions)}; ${field.description && field.description !== '' ? ` //${field.description}` : ''}`
